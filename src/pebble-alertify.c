e6b39dff712eb5b5f8387e77ecf90651de383ec4
Mr.Trogdan <mr.trogdan@gmail.com>
2014-03-16 17:55:46 -0400

    Added app message handlers, beginning of alert defs

--- src/pebble-alertify.c
+++ src/pebble-alertify.c
@@ -1,5 +1,20 @@
 #include "pebble.h"
 
+/////////////////////////////////////////////////////////////////////////////
+// Defines
+/////////////////////////////////////////////////////////////////////////////
+
+// Msg types
+#define MESSAGE_TYPE_ALERT  10
+
+// Msg Keys
+#define MESSAGE_KEY_TYPE    1000
+#define MESSAGE_KEY_TITLE   1001
+#define MESSAGE_KEY_BODY    1002
+
+/////////////////////////////////////////////////////////////////////////////
+// Globals
+/////////////////////////////////////////////////////////////////////////////
 static Window *g_root_window;
 
 // This is a scroll layer
@@ -18,6 +33,14 @@ static char g_splash_text[] = "Alertify";
 //static const int vert_scroll_text_padding = 4;
 
 //root window doubles as a splash screen, shown while waiting for alert from phone
+
+/////////////////////////////////////////////////////////////////////////////
+// Function declarations
+/////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+// Function definitions
+/////////////////////////////////////////////////////////////////////////////
 static void root_window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_frame(window_layer);
@@ -56,7 +79,52 @@ static void root_window_unload(Window *window) {
 	text_layer_destroy(g_primary_text_layer);
 }
 
+static void out_sent_handler(DictionaryIterator *sent, void *context) {
+   // outgoing message was delivered
+ }
+
+
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+   // outgoing message failed
+ }
+
+
+static void in_received_handler(DictionaryIterator *received, void *context) {
+	// incoming message received
+	Tuple *msg_tuple = dict_find(received, MESSAGE_KEY_TYPE);
+	
+	if(!msg_tuple) {
+		//TODO log unknown msg
+		return;
+	}
+	
+	Tuple *body_tuple = NULL;
+	Tuple *title_tuple = NULL;
+	
+	switch(msg_tuple->value->uint8)
+	{
+	case MESSAGE_TYPE_ALERT:
+		body_tuple = dict_find(received, MESSAGE_KEY_BODY);
+		title_tuple = dict_find(received, MESSAGE_KEY_TITLE);
+		
+		//show_alert(title, body);
+		break;
+	}
+ }
+
+
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+   // incoming message dropped
+ }
+
 int main(void) {
+	app_message_register_inbox_received(in_received_handler);
+	app_message_register_inbox_dropped(in_dropped_handler);
+	app_message_register_outbox_sent(out_sent_handler);
+	app_message_register_outbox_failed(out_failed_handler);
+	   
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	
 	g_root_window = window_create();
 	window_set_window_handlers(g_root_window, (WindowHandlers) {
 		.load = root_window_load,
